c0373b35d4d19853251298e43581007453adf58c
PharexAubin <94604216+PharexAubin@users.noreply.github.com>
2023-01-18 15:22:21 +0100

    paranthese

--- src/ContactService.php
+++ src/ContactService.php
@@ -18,8 +18,8 @@ class ContactService
      * ContactService constructor.
      * Initialise la BDD
      */
-    public function __construct(){
-        $this->pdo = new PDO('sqlite:' . __DIR__ . '/contacts.sqlite');
+    public function __construct()
+    { $this->pdo = new PDO('sqlite:' . __DIR__ . '/contacts.sqlite');
         $this->pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
         $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
@@ -30,20 +30,20 @@ class ContactService
      * @return mixed le retour de la requete SQL
      * @throws invalidInputException en cas d'erreur de paramètre
      */
-    public function getContact($id){
-        if (empty($id)){
+    public function getContact($id)
+        { if (empty($id)){
             throw new invalidInputException("l'id doit être renseigné");
         }
-        if (!is_numeric($id) || $id < 0){
-            throw new invalidInputException("l'id doit être un entier non nul");
+        if (!is_numeric($id) || $id < 0)
+            {  throw new invalidInputException("l'id doit être un entier non nul");
         }
         $req = $this->pdo->query('SELECT * from contacts where id =' . $id);
 
         $row = $req->fetchAll();
 
         // si req ok (!false)
-        if ($req) {
-            // on renvoie le 1er et seul élément du tableau de résultats
+        if ($req) 
+        {   // on renvoie le 1er et seul élément du tableau de résultats
             return $row[0];
         }
     }
@@ -54,12 +54,12 @@ class ContactService
      * @return array le retour de la requete SQL
      * @throws invalidInputException en cas d'erreur de paramètre
      */
-    public function searchContact($search){
-        if (empty($search)){
+    public function searchContact($search)
+        {  if (empty($search)){
             throw new invalidInputException('search doit être renseigné');
         }
-        if (!is_string($search)){
-            throw new invalidInputException('search doit être une chaine de caractères');
+        if (!is_string($search))
+        {  throw new invalidInputException('search doit être une chaine de caractères');
         }
         $req = "SELECT * from contacts where nom like '%" . $search . "%' or prenom like '%" . $search . "%'";
 
@@ -68,8 +68,8 @@ class ContactService
         $row = $res->fetchAll();
 
         // si req ok (!false)
-        if ($res){
-            return $row;
+        if ($res)
+            { return $row;
         }
     }
 
@@ -77,8 +77,8 @@ class ContactService
      * Récupère tous les contacts en BDD
      * @return array le retour de la requete SQL
      */
-    public function getAllContacts(){
-        $req = $this->pdo->query('SELECT * from contacts');
+    public function getAllContacts()
+        { $req = $this->pdo->query('SELECT * from contacts');
         $row = $req->fetchAll();
         // si req ok (!false)
         if ($req){
@@ -93,12 +93,12 @@ class ContactService
      * @return bool true si ok, false si erreur SQL
      * @throws invalidInputException en cas d'erreur de paramètre
      */
-    public function createContact($nom, $prenom){
-        if (empty($nom) && !is_string($nom)){
+    public function createContact($nom, $prenom)
+    {  if (empty($nom) && !is_string($nom)){
             throw new invalidInputException('le nom  doit être renseigné');
         }
-        if (empty($prenom) && !is_string($prenom)){
-            throw new invalidInputException('le prenom doit être renseigné');
+        if (empty($prenom) && !is_string($prenom))
+        { throw new invalidInputException('le prenom doit être renseigné');
         }
         $stmt = $this->pdo->prepare('INSERT INTO contacts (nom, prenom) VALUES (:nom, :prenom)');
         return $stmt->execute([
@@ -115,19 +115,19 @@ class ContactService
      * @return bool true si ok, false si erreur SQL
      * @throws invalidInputException en cas d'erreur de paramètre
      */
-    public function updateContact($id, $nom, $prenom){
-        if (empty($nom) && !is_string($nom)){
-            throw new invalidInputException('le nom  doit être renseigné');
+    public function updateContact($id, $nom, $prenom)
+    {  if (empty($nom) && !is_string($nom))
+        {  throw new invalidInputException('le nom  doit être renseigné');
         }
 
-        if (empty($id)){
-            throw new invalidInputException("l'id doit être renseigné");
+        if (empty($id))
+        { throw new invalidInputException("l'id doit être renseigné");
         }
-        if (!is_numeric($id) || $id < 0){
-            throw new invalidInputException("l'id doit être un entier non nul");
+        if (!is_numeric($id) || $id < 0)
+        {    throw new invalidInputException("l'id doit être un entier non nul");
         }
-        if (empty($prenom) && !is_string($prenom)){
-            throw new invalidInputException('le prenom doit être renseigné');
+        if (empty($prenom) && !is_string($prenom))
+        {  throw new invalidInputException('le prenom doit être renseigné');
         }
         $stmt = $this->pdo->prepare('UPDATE contacts SET nom=:nom, prenom=:prenom where id=:id');
 
@@ -144,12 +144,12 @@ class ContactService
      * @return bool true si SQL ok, false si non
      * @throws invalidInputException en cas d'erreur de paramètre
      */
-    public function deleteContact($id){
-        if (null === $id){
+    public function deleteContact($id)
+    {  if (null === $id){
             throw new invalidInputException("l'id doit être renseigné");
         }
-        if (!is_numeric($id) || $id < 0){
-            throw new invalidInputException("l'id doit être un entier non nul");
+        if (!is_numeric($id) || $id < 0)
+        { throw new invalidInputException("l'id doit être un entier non nul");
         }
         $stmt = $this->pdo->prepare('DELETE from contacts where id=:id');
 
@@ -162,7 +162,7 @@ class ContactService
      * Supprime tous les contacts
      * @return false|PDOStatement
      */
-    public function deleteAllContact(){
-        return $this->pdo->query('DELETE from contacts');
+    public function deleteAllContact()
+    {  return $this->pdo->query('DELETE from contacts');
     }
 }
