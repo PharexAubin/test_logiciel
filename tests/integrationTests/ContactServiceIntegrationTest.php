f6477933184dd3ca67ae4f5dbb4d4ae682f65235
PharexAubin <94604216+PharexAubin@users.noreply.github.com>
2023-01-25 11:02:01 +0100

    s

--- tests/integrationTests/ContactServiceIntegrationTest.php
+++ tests/integrationTests/ContactServiceIntegrationTest.php
@@ -36,6 +36,7 @@ final class ContactServiceIntegrationTest extends TestCase
     {
         $contactService = new ContactService();
         $contactService->deleteAllContact();
+        $this->assertEquals(0, $contactService->countContacts());
     }
 
 
