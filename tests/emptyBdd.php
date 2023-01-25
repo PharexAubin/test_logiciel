5ede0bdf10b8f8f5cc823bbcc7054d9b0c2d8ba2
PharexAubin <94604216+PharexAubin@users.noreply.github.com>
2023-01-25 09:44:06 +0100

    solar

--- tests/emptyBdd.php
+++ tests/emptyBdd.php
@@ -1,15 +1,11 @@
 <?php
 
 /*
- * This file is part of the Symfony package.
- *
- * (c) Fabien Potencier <fabien@symfony.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
+ * require_once __DIR__.'/../src/contacts.php';
  */
 
-require_once __DIR__.'/../src/contacts.php';
+
+use  src\Contacts;
 
 $contacts = new contactService();
 $contacts->init('contactsTest.sqlite');
