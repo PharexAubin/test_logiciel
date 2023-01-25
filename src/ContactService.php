ece4e093ce3740a26b44926ebd3b362c939d31c1
PharexAubin <94604216+PharexAubin@users.noreply.github.com>
2023-01-25 08:48:36 +0100

    solar

--- src/ContactService.php
+++ src/ContactService.php
@@ -18,7 +18,8 @@ class ContactService
      * ContactService constructor.
      * Initialise la BDD
      */
-    public function __construct() { $this->pdo = new PDO('sqlite:' . __DIR__ . '/contacts.sqlite');
+    public function __construct()
+    { $this->pdo = new PDO('sqlite:' . __DIR__ . '/contacts.sqlite');
         $this->pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
         $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
